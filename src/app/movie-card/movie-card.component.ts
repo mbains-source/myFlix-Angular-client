74f3da32edf9353e247874c86757c9637f4bc1cc
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-12-18 21:26:16 -0800

    implement movie card

---
+++ src/app/movie-card/movie-card.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.scss']
+})
+export class MovieCardComponent implements OnInit {
+
+  movies : any[] = [];
+
+  constructor(public fetchApi: FetchApiDataService) { }
+
+  ngOnInit(): void {
+    this.getAllMovies();
+  }
+
+  public getAllMovies(): void {
+    this.fetchApi.getAllMovies().subscribe((response: any) => {
+      this.movies = response;
+      console.log(response);
+      return this.movies;
+    });
+  }
+
+}
